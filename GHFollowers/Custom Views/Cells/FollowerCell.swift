8c0f1b04b2a5ea39ff8744f8e31fd5ec007b0548
Edson Brandon <edsonbrandon8@gmail.com>
2024-12-02 10:45:35 -0300

    feat: followers cell

---
+++ GHFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,12 @@
+//
+//  FollowerCell.swift
+//  GHFollowers
+//
+//  Created by Edson Brandon on 02/12/24.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+}
---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,20 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Edson Brandon on 02/12/24.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
