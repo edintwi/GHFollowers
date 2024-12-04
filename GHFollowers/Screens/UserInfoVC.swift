44732963cc5d418de5fe72955ec72e0117d180bf
Edson Brandon <edsonbrandon8@gmail.com>
2024-12-04 10:39:46 -0300

    feat: get user info network call

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class UserInfoVC: UIViewController {
 
-    var username: String?
-    
+    var username: String!
+    var user: User!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +18,18 @@ class UserInfoVC: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+                print(error)
+            }
+        }
     }
     
     @objc func dismissVC() {
