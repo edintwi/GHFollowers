c24ff9f61520c5461a2e4d59471abc9b54908696
Edson Brandon <edsonbrandon8@gmail.com>
2024-12-06 09:42:10 -0300

    feat: adding add button

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -42,6 +42,9 @@ class FollowerListVC: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton                     = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
     }
     
     func configureCollectionView() {
@@ -103,6 +106,11 @@ class FollowerListVC: UIViewController {
         }
         
     }
+    
+    @objc func addButtonTapped() {
+        print("Add button tapped")
+    }
+    
     
 }
 
